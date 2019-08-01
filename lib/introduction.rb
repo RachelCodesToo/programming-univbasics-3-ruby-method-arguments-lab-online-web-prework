df679fa6556d9844587f3809af0ceaa2b4056dce
Anne Fausto <a.r.fausto@gmail.com>
2019-08-01 12:24:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,3 @@
-def #intoduction
-name = "Josh"
-puts "Hi, my name is #{name}."
-name = "Dan"
-puts "Hi, my name is #{name}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
-
-def #introduction_with_language
-name = "Josh"
-language= "Ember.js"
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end 
